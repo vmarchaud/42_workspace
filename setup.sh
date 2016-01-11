6d280fad2b8bc8893ccded185b18c6f82c1f305e
Valentin MARCHAUD <vmarchau@e1r8p5.42.fr>
2016-01-11 11:57:51 +0100

    setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+curl http://www.iterm2.com/nightly/iTerm2-2_9_20160108-nightly.zip > ~/Downloads/Iterm.zip
+open ~/Downloads/Iterm.zip
+curl -L http://install.ohmyz.sh | sh
+curl https://raw.githubusercontent.com/reversTeam/Sublivim/master/installer.sh | sh 
+curl https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim > ~/.vim/colors/monokai.vim
+sed -i -- 's/molokai/monokai/g' ~/.vimrc
+curl https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.zsh-theme > ~/.oh-my-zsh/themes/honukai.zsh-theme
+sed -i -- 's/ZSH_THEME="robyrussell"/ZSH_THEME="honukai"' ~/.zshrc 
+source ~/.zshrc
+curl https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.itermcolors > ~/Downloads/honukai.itermcolors
+open ~/Downloads/iTerm.app/ 
