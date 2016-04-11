59615b882bc019409e6974bc44a50c76f27a4593
Valentin ThisIsMac M <thisismac47@gmail.com>
2016-04-11 15:28:17 +0200

    fix

--- piscine/php/j05/ex01/ex01.sql
+++ piscine/php/j05/ex01/ex01.sql
@@ -1,10 +1,8 @@
-use db_vmarchau;
-
 CREATE TABLE IF NOT EXISTS ft_table
 (
-	id INTEGER NOT NULL AUTO_INCREMENT,
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	login VARCHAR(8) DEFAULT 'toto',
 	groupe ENUM('staff', 'student', 'other') NOT NULL,
 	date_de_creation DATE NOT NULL
 
-)
+);
--- piscine/php/j05/ex03/ex03.sql
+++ piscine/php/j05/ex03/ex03.sql
@@ -1,1 +1,1 @@
-INSERT INTO ft_table (login, date_de_creation) SELECT (nom, date_naissance) FROM fiche_personne WHERE nom LIKE '%a%' AND LENGTH(nom) < 9 ORDER BY nom LIMIT 10;
+INSERT INTO ft_table (login, date_de_creation) SELECT nom,date_naissance FROM fiche_personne WHERE nom LIKE '%a%' AND LENGTH(nom) < 9 ORDER BY nom LIMIT 10;
