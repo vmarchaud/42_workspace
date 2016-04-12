5b8cbf311fa5ec4501b52ba0a728c04146458b75
Valentin ThisIsMac M <thisismac47@gmail.com>
2016-04-12 17:15:58 +0200

    update ex01 j06

--- piscine/php/j06/ex01/Vertex.class.php
+++ piscine/php/j06/ex01/Vertex.class.php
@@ -7,27 +7,65 @@ class Vertex {
 	private $_x = 0.0;
 	private $_y = 0.0;
 	private $_z = 0.0;
-	private $_w = 0.0;
+	private $_w = 1.0;
 	private $_color;
 
 	static $verbose = false;
+	function __toString() {
+		return	sprintf("Vertex ( x: %d, y: %d, z: %d, w: %d, %s )", $this->_x, $this->_y, $this->_z, $this->_w, $this->_color);	
+	}
 
 	function __construct( $kwargs ) {
 		if (isset($kwargs['color']))
-			$this->color = $kwargs['color'];
+			$this->_color = $kwargs['color'];
 		else
-			$this->color = new Color( array(rgb => 0xffffff));
+			$this->_color = new Color( array("rgb" => 0xffffff));
 		if (isset($kwargs['x']))
-			$this->x = $kwargs['x'];
+			$this->_x = $kwargs['x'];
 		if (isset($kwargs['y']))
-			$this->y = $kwargs['y'];
+			$this->_y = $kwargs['y'];
 		if (isset($kwargs['z']))
-			$this->z = $kwargs['z'];
-		if (Vertex::verbose)
+			$this->_z = $kwargs['z'];
+		if (isset($kwargs['w']))
+			$this->_w = $kwargs['w'];
 
+		if (Vertex::$verbose)
+			echo $this . " constructed\n";
 	}
 
+	function __destruct() {
+		if (Vertex::$verbose)
+			echo $this . " destructed\n";
+	}
+	
+	function __get($random) {
+		if (Vertex::$verbose)
+			echo "You must use the public getter";
+		return "";
+	}
+	
+	function __set($random, $value) {
+		if (Vertex::$verbose)
+			echo "You must use the public setter";
+	}
 
+	function getColor() { return $this->_color; }
+	function getX() { return $this->_x; }
+	function getY() { return $this->_y; }
+	function getZ() { return $this->_z; }
+	function getW() { return $this->_w; }
+
+	function setColor($color) { $this->_color = $color; }
+	function setX($x) { $this->_x = $x; }
+	function setY($y) { $this->_y = $y; }
+	function setZ($z) { $this->_z = $z; }
+	function setW($w) { $this->_w = $w; }
+	
+
+	static function doc() {
+		$content = file_get_contents("./Vertex.doc.txt");
+		echo $content;
+	}
 }
 
 ?>
